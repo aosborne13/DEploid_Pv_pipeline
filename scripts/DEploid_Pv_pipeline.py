6b691f318a5df060c1e3b47d8b4b1fa7c8d3069c
Ashley Osborne <57442176+aosborne13@users.noreply.github.com>
2024-03-14 14:17:46 +0000

    Update DEploid_Pv_pipeline.py

--- scripts/DEploid_Pv_pipeline.py
+++ scripts/DEploid_Pv_pipeline.py
@@ -21,15 +21,16 @@ def run_cmd(cmd):
     if res!=0:
         sys.exit("Error running last command, please check!\n")
 
+def main(args)
 # requires filtered vcf
 # extract snps and filter by missingness
-run_cmd('bcftools view -v snps %(vcf)s -Oz -o merged_snps.vcf.gz' % vars(args))
-run_cmd("bcftools filter -i 'FMT/DP>4' -S . merged_snps.vcf.gz -Oz -o merged.filt.snps.vcf.gz")
-run_cmd('plink --const-fid --vcf merged.filt.snps.vcf.gz --mind %(miss)s --recode vcf --allow-extra-chr --out merged_plink' % vars(args))
-run_cmd("grep -P \"^#CHROM\" merged_plink.vcf | awk '{ $1=\"\"; $2=\"\";$3=\"\"; $4=\"\";$5=\"\"; $6=\"\";$7=\"\"; $8=\"\";$9=\"\"; print}' | sed 's/ /\\n/g' | tail -n+10 | sed 's/^0_//'  > merged_plink_new")
-run_cmd("bcftools view -S mhaps_merged_plink_new --threads 4 -O z -o  merged.miss0.4.filt.snps.vcf.gz merged.filt.snps.vcf.gz")
-run_cmd("bcftools view merged.miss0.4.filt.snps.vcf.gz | setGT.py --fraction 0.8 | bcftools view -O z -c 1 -o merged.filt.GT.miss0.4.snps.vcf.gz")
-run_cmd("bcftools view -m2 -M2 merged.filt.GT.miss0.4.snps.vcf.gz --threads 4 -O z -o merged.bi.filt.GT.miss0.4.snps.vcf.gz")
+    run_cmd('bcftools view -v snps %(vcf)s -Oz -o merged_snps.vcf.gz' % vars(args))
+    run_cmd("bcftools filter -i 'FMT/DP>4' -S . merged_snps.vcf.gz -Oz -o merged.filt.snps.vcf.gz")
+    run_cmd('plink --const-fid --vcf merged.filt.snps.vcf.gz --mind %(miss)s --recode vcf --allow-extra-chr --out merged_plink' % vars(args))
+    run_cmd("grep -P \"^#CHROM\" merged_plink.vcf | awk '{ $1=\"\"; $2=\"\";$3=\"\"; $4=\"\";$5=\"\"; $6=\"\";$7=\"\"; $8=\"\";$9=\"\"; print}' | sed 's/ /\\n/g' | tail -n+10 | sed 's/^0_//'  > merged_plink_new")
+    run_cmd("bcftools view -S mhaps_merged_plink_new --threads 4 -O z -o  merged.miss0.4.filt.snps.vcf.gz merged.filt.snps.vcf.gz")
+    run_cmd("bcftools view merged.miss0.4.filt.snps.vcf.gz | setGT.py --fraction 0.8 | bcftools view -O z -c 1 -o merged.filt.GT.miss0.4.snps.vcf.gz")
+    run_cmd("bcftools view -m2 -M2 merged.filt.GT.miss0.4.snps.vcf.gz --threads 4 -O z -o merged.bi.filt.GT.miss0.4.snps.vcf.gz")
 
 # use filtered VCF for DEploid
 
@@ -37,6 +38,8 @@ run_cmd("bcftools view -m2 -M2 merged.filt.GT.miss0.4.snps.vcf.gz --threads 4 -O
 parser = argparse.ArgumentParser(description='DEploid pipeline wrapper for use on P. vivax',formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('--vcf',type=str,help='Merged vcf file containing all required samples; Use zipped format, i.e. vcf.gz',required=True)
 parser.add_argument('--miss', default=0.4,type=int,help='Percentage missingness allowed; 0.4 allows 40% missingness')
+parser.add_argument('--version', action='version', version='%(prog)s 1.0')
+parser.set_defaults(func=main)
 
 args = parser.parse_args()
 args.func(args)
