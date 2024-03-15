46968ab298baf4850759d889bd194af802f1ec12
Ashley Osborne <57442176+aosborne13@users.noreply.github.com>
2024-03-15 12:59:22 +0000

    Update DEploid_Pv_pipeline.py

--- scripts/DEploid_Pv_pipeline.py
+++ scripts/DEploid_Pv_pipeline.py
@@ -31,7 +31,7 @@ def main(args):
     run_cmd("bcftools view -m2 -M2 merged.filt.GT.miss0.4.snps.vcf.gz --threads 4 -O z -o merged.bi.filt.GT.miss0.4.snps.vcf.gz")
 
     run_cmd("bash extract_DEploid.sh")
-    run_cmd("find /DEploid_input/merged.bi.filt.GT.miss0.4.snps.GT.txt.gz -print0 | xargs -0 -n 1 -P 1 Rscript ~/tools/DEploid_Pv_pipeline/scripts/calculate_PLAF_DEploid.R")
+    run_cmd("find DEploid_input/merged.bi.filt.GT.miss0.4.snps.GT.txt.gz -print0 | xargs -0 -n 1 -P 1 Rscript ~/tools/DEploid_Pv_pipeline/scripts/calculate_PLAF_DEploid.R")
 
 # use filtered VCF for DEploid
 # Set up the parser
