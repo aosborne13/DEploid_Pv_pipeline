d23cd2517192e01e24100b9acfb4a7f9c52ba3b4
Ashley Osborne <57442176+aosborne13@users.noreply.github.com>
2024-03-18 12:17:03 +0000

    Update subset_sites.sh

--- scripts/subset_sites.sh
+++ scripts/subset_sites.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-sites='/DEploid_input/merged_dataset.filt.snps.PLAF.sites.txt'
+sites='/DEploid_input/merged.bi.filt.GT.miss0.4.snps.PLAF.sites.txt'
 
 bcftools view \
 	--output /DEploid_input/merged_dataset.filt.DEploid.bcf.gz \
 	--output-type b \
 	--threads 8 \
 	--regions-file "$sites" \
-	/DEploid_input/merged_dataset.filt.snps.vcf.gz
+	merged.bi.filt.GT.miss0.4.snps.vcf.gz
