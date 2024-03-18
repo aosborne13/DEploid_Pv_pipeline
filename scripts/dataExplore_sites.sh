8a3b47e2142387d6d10db7178ad41ca0c2234890
Ashley Osborne <57442176+aosborne13@users.noreply.github.com>
2024-03-18 13:33:46 +0000

    Update dataExplore_sites.sh

--- scripts/dataExplore_sites.sh
+++ scripts/dataExplore_sites.sh
@@ -4,7 +4,7 @@ plaf='DEploid_input/merged.bi.filt.GT.miss0.4.snps.PLAF.sites.txt'
 samples='samples.txt'
 
 while read -r sample; do
-    /home/ashley/tools/DEploid/utilities/dataExplore.r \
+    ~/tools/DEploid/utilities/dataExplore.r \
         -ref "DEploid_input/$sample.AD.0.txt.gz" \
         -alt "DEploid_input/$sample.AD.1.txt.gz" \
         -plaf "$plaf" \
