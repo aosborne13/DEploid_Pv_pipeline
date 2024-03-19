ba3efa0385a56eac76f737befa498c82ceb50cf2
Ashley Osborne <57442176+aosborne13@users.noreply.github.com>
2024-03-19 11:29:21 +0000

    Update DEploid_Pv_pipeline.py

--- scripts/DEploid_Pv_pipeline.py
+++ scripts/DEploid_Pv_pipeline.py
@@ -42,6 +42,7 @@ def main(args):
     run_cmd("mkdir BEST")
     run_cmd("Rscript ~/tools/DEploid_Pv_pipeline/scripts/resolve_mixed_sites.R")
     run_cmd("Rscript ~/tools/DEploid_Pv_pipeline/scripts/create_BEST_reference_panel.R")
+    run_cmd("bash run_DEploid_BEST_sites.sh")
 
 # use filtered VCF for DEploid
 # Set up the parser
